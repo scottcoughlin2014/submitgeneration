022622d60e9dae98bbb919867ce9f849ed7b408f
scottcoughlin2014 <scottcoughlin2014@u.northwestern.edu>
2016-01-22 13:06:27 -0600

    Updating submit script to show how one could create all the freezing params combo runs with one bash loop.

--- runsubmit.sh
+++ runsubmit.sh
@@ -7,7 +7,19 @@ source /projects/b1011/ligo_project/lsc/o1_lalinference_20151210/etc/lscsoftrc
 
 rm -rf ./condor
 
-for event in {0..5};
-	do ./lalinference_mcmc_submit_new.py --inj /projects/b1011/spinning_runs/STT4injections.xml --event ${event} --approx SpinTaylorT4 --lowM1 5.0 --lowM2 1.0 --dir /projects/b1011/scoughlin/test/${event}; 
-done;
-# --fix-distance --fix-rightascension --fix-distance --fix-costheta_jn
+for event in {0 11 44 101};
+	do for combo in "none" "skyloc" "skyloc_dist" "skyloc_thetajn" "skyloc_thetajn_dist";
+		if [ ${combo} = "skyloc" ]; then
+                   addflags = "--fix-rightascension --fix-declination"
+		else if [ ${combo} = "skyloc_dist" ]; then
+		   addflags = "--fix-rightascension --fix-declination --fix-distance"
+                else if [ ${combo} = "skyloc_thetajn_dist" ]; then
+		   addflags = "--fix-rightascension --fix-declination --fix-costheta_jn"
+                else if [ ${combo} = "skyloc__thetajn_dist" ]; then
+                   addflags = "--fix-rightascension --fix-declination --fix-costheta_jn --fix-distance"
+                else
+		   addflags = ""
+                fi
+		do ./lalinference_mcmc_submit_new.py --inj /projects/b1011/spinning_runs/STT4injections.xml --event ${event} --approx SpinTaylorT4 --lowM1 5.0 --lowM2 1.0 --dir /projects/b1011/spinning_runs/freezingparams/${event}/${combo} ${addflags};
+		done;
+# --fix-distance --fix-rightascension --fix-declination --fix-costheta_jn
