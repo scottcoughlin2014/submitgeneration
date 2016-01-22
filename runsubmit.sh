e5d14e3dd44895818e60386e6345363b25cc4200
scottcoughlin2014 <scottcoughlin2014@u.northwestern.edu>
2016-01-22 11:40:18 -0600

    Fixing example submit

--- runsubmit.sh
+++ runsubmit.sh
@@ -5,5 +5,5 @@ module load mpi/openmpi-1.8.3-intel2015.0
 
 source /projects/b1011/ligo_project/lsc/o1_lalinference_20151210/etc/lscsoftrc
 
-./lalinference_mcmc_submit_new.py --inj /projects/b1011/spinning_runs/STT4injections.xml --event 11 --approx SpinTaylorT4 --lowM1 1.0 --lowM2 5.0 --dir "Path to submit directory" 
+./lalinference_mcmc_submit_new.py --inj /projects/b1011/spinning_runs/STT4injections.xml --event 11 --approx SpinTaylorT4 --lowM1 5.0 --lowM2 1.0 --dir /projects/b1011/scoughlin/test/ 
 # --fix-distance --fix-rightascension --fix-distance --fix-costheta_jn
