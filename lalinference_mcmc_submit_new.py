24322439d93e76caba4046d5056c003f694545eb
scottcoughlin2014 <scottcoughlin2014@u.northwestern.edu>
2016-02-18 21:38:25 -0600

    More updating

--- lalinference_mcmc_submit_new.py
+++ lalinference_mcmc_submit_new.py
@@ -642,7 +642,7 @@ with open(ppFilePath,'w') as ppfile:
         ppfile.write('\n')
 
         ppfile.write('source /projects/b1011/non-lsc/lscsoft-user-env.sh\n')
-        ppfile.write('source /projects/b1011/ligo_project/lsc/o1_lalinference_20151210-3-gcee9c5e/etc/lscsoftrc\n')
+        ppfile.write('source /projects/b1011/ligo_project/lsc/o1_lalinference_20151210_v2/etc/lscsoftrc\n')
         ppfile.write('\n')
 
         ppfile.write('cbcBayesPostProc.py --lalinfmcmc -i {} --event {} --outpath={} -d {}/PTMCMC.output.*.00 --dievidence  --skyres=.5 --deltaLogL {}\n'.format(args.inj,args.event,webdir,out_dir,target_hot_like))
