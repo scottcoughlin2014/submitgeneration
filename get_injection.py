Fixing the calculation of costhetajn

get_injection.py
@@ -3,17 +3,28 @@
 import argparse
 import numpy as np
 from pylal import SimInspiralUtils
-
+import lal
+import lalsimulation as lalsim
 
 parser = argparse.ArgumentParser()
 parser.add_argument("--inj", dest="inj", default=None, help="Injection XML Path")
 parser.add_argument("--event", dest ="event", type=int, default=None, help="Event Number")
-
+parser.add_argument("--frame", dest ="frame", default="OrbitalL", help="Spin Frame of injection")
 args = parser.parse_args()
 
 injname = args.inj
 event = args.event
+frame = args.frame
 
+def cart2sph(x,y,z):
+    """
+    Utility function to convert cartesian coords to r,theta,phi.
+    """
+    r = np.sqrt(x*x + y*y + z*z)
+    theta = np.arccos(z/r)
+    phi = np.fmod(2*np.pi + np.arctan2(y,x), 2*np.pi)
+
+    return r,theta,phi
 
 def ROTATEZ(angle, vx, vy, vz):
     # This is the ROTATEZ in LALSimInspiral.c.
@@ -36,8 +47,7 @@ def orbital_momentum(fref, mc, inclination):
     Note that if one wants to build J=L+S1+S2 with L returned by this function, S1 and S2
     must not get the Msun^2 factor.
     """
-    from lal import MTSUN_SI
-    Lmag = np.power(mc, 5.0/3.0) / np.power(np.pi * MTSUN_SI * fref, 1.0/3.0)
+    Lmag = np.power(mc, 5.0/3.0) / np.power(np.pi * lal.MTSUN_SI * fref, 1.0/3.0)
     Lx, Ly, Lz = sph2cart(Lmag, inclination, 0.0)
     return np.hstack((Lx,Ly,Lz))
 
@@ -109,16 +119,27 @@ def extract_inj_vals(sim_inspiral_event):
     return injvals
 
 def calculate_injected_sys_frame_params(sim_inspiral_event, f_ref = 100.0):
+    # Taken from pylal bayespputils.py
+    axis = lalsim.SimInspiralGetFrameAxisFromString(frame)
+
+    m1, m2 = sim_inspiral_event.mass1, sim_inspiral_event.mass2
+    m1 *= lal.MSUN_SI
+    m2 *= lal.MSUN_SI
+    mc, eta = sim_inspiral_event.mchirp, sim_inspiral_event.eta
+
+    # Convert to radiation frame
+    iota, s1x, s1y, s1z, s2x, s2y, s2z = \
+          lalsim.SimInspiralInitialConditionsPrecessingApproxs(sim_inspiral_event.inclination,sim_inspiral_event.spin1x, sim_inspiral_event.spin1y, sim_inspiral_event.spin1z, sim_inspiral_event.spin2x, sim_inspiral_event.spin2y, sim_inspiral_event.spin2z, m1, m2, f_ref, axis)
+
+    a1, theta1, phi1 = cart2sph(s1x, s1y, s1z)
+    a2, theta2, phi2 = cart2sph(s2x, s2y, s2z)
+
     L  = orbital_momentum(f_ref, sim_inspiral_event.mchirp, sim_inspiral_event.inclination)
-    S1 = np.hstack((sim_inspiral_event.spin1x, sim_inspiral_event.spin1y, sim_inspiral_event.spin1z))
-    S2 = np.hstack((sim_inspiral_event.spin2x, sim_inspiral_event.spin2y, sim_inspiral_event.spin2z))
+    S1 = np.hstack((s1x, s1y, s1z))
+    S2 = np.hstack((s2x, s2y, s2z))
 
-    a1 = np.sqrt(np.sum(S1 * S1))
-    a2 = np.sqrt(np.sum(S2 * S2))
-
-    S1 *= sim_inspiral_event.mass1*sim_inspiral_event.mass1
-    S2 *= sim_inspiral_event.mass2*sim_inspiral_event.mass2
-
+    S1 *= m1**2
+    S2 *= m2**2
     J = L + S1 + S2
 
     tilt1 = array_ang_sep(L, S1) if not all([i==0.0 for i in S1]) else 0.0
@@ -134,17 +155,15 @@ def calculate_injected_sys_frame_params(sim_inspiral_event, f_ref = 100.0):
     else:
         spin2z = a2 * np.cos(tilt2)
 
-    theta_jn = array_polar_ang(J)
-
     # Need to do rotations of XLALSimInspiralTransformPrecessingInitialConditioin inverse order to go in the L frame
     # first rotation: bring J in the N-x plane, with negative x component
     phi0 = np.arctan2(J[1], J[0])
     phi0 = np.pi - phi0
-
     J = ROTATEZ(phi0, J[0], J[1], J[2])
     L = ROTATEZ(phi0, L[0], L[1], L[2])
     S1 = ROTATEZ(phi0, S1[0], S1[1], S1[2])
     S2 = ROTATEZ(phi0, S2[0], S2[1], S2[2])
+    theta_jn = array_polar_ang(J)
 
     # now J in in the N-x plane and form an angle theta_jn with N, rotate by -theta_jn around y to have J along z
     J = ROTATEY(theta_jn,J[0],J[1],J[2])
@@ -177,7 +196,6 @@ def calculate_injected_sys_frame_params(sim_inspiral_event, f_ref = 100.0):
         phi12 = phi2 - phi1
 
     return a1, a2, spin1z, spin2z, theta_jn, phi_jl, tilt1, tilt2, phi12
-
 
 ###### DO STUFF
 