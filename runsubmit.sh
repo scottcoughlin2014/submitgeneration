767a3820a49e4b73769657511be11c59b03fdb1b
Scott Coughlin <scott.coughlin@ligo.org>
2016-04-15 14:20:47 -0500

    Updating run submit example to have STT4 and the new ppall flag for JEssie's BNS runs

--- runsubmit.sh
+++ runsubmit.sh
@@ -8,6 +8,6 @@ source /projects/b1011/ligo_project/lsc/o1_lalinference_20160402/etc/lscsoftrc
 rm -rf ./condor
 
 for event in 11 44;
-		do ./lalinference_mcmc_submit_new.py --inj /projects/b1011/ligo_project/full_bns.xml --event ${event} --approx SpinTaylorT2 --lowM1 1.0 --lowM2 1.0 --dir /projects/b1011/spinning_runs/BNS/${event}/ --comp-min 1.0 --comp-max 5.0
+		do ./lalinference_mcmc_submit_new.py --inj /projects/b1011/ligo_project/full_bns.xml --event ${event} --approx SpinTaylorT4 --lowM1 1.0 --lowM2 1.0 --dir /projects/b1011/spinning_runs/BNS/STT4/${event}/ --comp-min 1.0 --comp-max 5.0 --ppall
 done;
 # --fix-distance --fix-rightascension --fix-declination --fix-costheta_jn
